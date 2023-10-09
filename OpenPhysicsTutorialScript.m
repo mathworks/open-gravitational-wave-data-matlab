d2e1a26ca0082d534afd443d0b951dbc12b359d8
Shubo Chakrabarti <shuboc@mathworks.com>
2023-10-09 18:05:56 +0200

    added acknowledgements per CC license requirements

--- OpenPhysicsTutorialScript.m
+++ OpenPhysicsTutorialScript.m
@@ -550,3 +550,21 @@ end
 % "Open data from the first and second observing runs of Advanced LIGO and Advanced 
 % Virgo", <https://doi.org/10.1016/j.softx.2021.100658 SoftwareX 13 (2021) 100658> 
 % -- <https://inspirehep.net/literature/1773351 INSPIRE>
+
+%% Acknowledgements
+%This research has made use of data or software obtained from the Gravitational Wave 
+% Open Science Center (gwosc.org), a service of the LIGO Scientific Collaboration, 
+% the Virgo Collaboration, and KAGRA. This material is based upon work supported by 
+% NSF's LIGO Laboratory which is a major facility fully funded by the National Science 
+% Foundation, as well as the Science and Technology Facilities Council (STFC) of the 
+% United Kingdom, the Max-Planck-Society (MPS), and the State of Niedersachsen/Germany 
+% for support of the construction of Advanced LIGO and construction and operation of the 
+% GEO600 detector. Additional support for Advanced LIGO was provided by the Australian 
+% Research Council. Virgo is funded, through the European Gravitational Observatory (EGO), 
+% by the French Centre National de Recherche Scientifique (CNRS), the Italian Istituto 
+% Nazionale di Fisica Nucleare (INFN) and the Dutch Nikhef, with contributions by 
+% institutions from Belgium, Germany, Greece, Hungary, Ireland, Japan, Monaco, Poland, 
+% Portugal, Spain. KAGRA is supported by Ministry of Education, Culture, Sports, Science 
+% and Technology (MEXT), Japan Society for the Promotion of Science (JSPS) in Japan; 
+% National Research Foundation (NRF) and Ministry of Science and ICT (MSIT) in Korea; 
+% Academia Sinica (AS) and National Science and Technology Council (NSTC) in Taiwan
